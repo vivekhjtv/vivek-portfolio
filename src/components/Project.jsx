bd649a64d232fbdbf58ce2e3e71b37c54011723b
Vivek Jethva <vivekhjtv18@gmail.com>
2024-02-09 19:46:25 -0500

    I have change card height for the same size.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,7 +5,14 @@ function Project({ card }) {
   return (
     <div>
       <Link to={card.link} target="_blank" style={{ textDecoration: 'none' }}>
-        <div className="card h-100" style={{ marginBottom: 24 }}>
+        <div
+          className="card"
+          style={{
+            marginBottom: 24,
+            height: 512,
+            overflow: 'hidden',
+          }}
+        >
           <img src={card.card_image} className="card-img-top" alt="..." />
           <div className="card-body">
             <h4>{card.card_title}</h4>
